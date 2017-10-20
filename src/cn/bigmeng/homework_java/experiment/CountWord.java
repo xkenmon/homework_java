a961ee1b04f068b703f4521e898be26f86cc8598
XiangkunMeng <947139299@qq.com>
2017-10-20 09:38:10 +0800

    close stream.

--- src/cn/bigmeng/homework_java/experiment/CountWord.java
+++ src/cn/bigmeng/homework_java/experiment/CountWord.java
@@ -13,6 +13,7 @@ public class CountWord {
         System.out.println("输入一段文字，单独一行\"EOF\"结束输入，程序将统计输入的单词数量：");
         for (String one = ""; !(one=sc.nextLine()).equals("EOF"); all += one+"\n")
             ;
+        sc.close();
         String[] arr = all.split("\\W+");
         System.out.println("总字符数：" + arr.length);
         for (String s : arr) {
